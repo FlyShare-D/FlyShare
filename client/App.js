fc9754d26e940818d5955dcad7151c8cb9af2564
chunhz <chz062009@gmail.com>
2022-10-17 16:01:03 -0400

    webpack config finished

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import './styles.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello world</header>
+    </div>
+  );
+}
+
+export default App;
