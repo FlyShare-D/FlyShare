a33dbcba11d0d9ca3fc4485612972b67f1508be6
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-10-18 17:48:58 -0400

    add flights Container

--- client/src/flightsContainer.js
+++ client/src/flightsContainer.js
@@ -1,14 +1,67 @@
 import React from 'react';
-// import "../styles.css"
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
 
-function flightsContainer () {
+const FlightsContainer = () => {
+  const flights = [{destination: "Germany", flightName: "Delta", price: 250}, {destination: "Germany", flightName: "American Airlines", price: 350}];
+  const listItems = [];
+  for (const flight of flights) {
+    listItems.push(
+      <ListItem disablePadding>
+        <ListItemText primary={flight.flightName} />
+        <Box>
+          
+        </Box>
+      </ListItem>
+    )
+  }
   return (
-    <div className="containerFlights">
-      <div className="outerBox">
-        <h3 id="header"> Flights </h3>
-      </div>
-    </div>
-  )
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          {listItems}
+          {/* <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem> */}
+        </List>
+      </nav>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Trash" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
 }
 
-export default flightsContainer;
+export default FlightsContainer;
